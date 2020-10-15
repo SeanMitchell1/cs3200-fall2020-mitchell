58984e30ebcad9de184862e24d01068612d540b7
SeanMitchell1 <seanjm77@gmail.com>
2020-10-15 19:51:12 -0400

    first commit

---
+++ src/main/java/edu/northeastern/cs3200/controllers/hello/HelloController.java
@@ -0,0 +1,18 @@
+package edu.northeastern.cs3200.controllers.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/api/hello/string")
+	public String sayHello() {
+		return "Hello Sean Mitchell!";
+	}
+	@RequestMapping("/api/hello/object")
+	public HelloObject sayHelloObject() {
+		HelloObject obj = new HelloObject("Hello Sean Mitchell!");
+		return obj;
+	}
+
+}
